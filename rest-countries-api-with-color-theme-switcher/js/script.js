a81fa6761b7a53f9d6e7c25f06ca598f6e18c84d
popehiflo <poolpetterhijuel@hotmail.com>
2022-10-28 21:22:39 -0500

    feat: JS, call to API

--- rest-countries-api-with-color-theme-switcher/js/script.js
+++ rest-countries-api-with-color-theme-switcher/js/script.js
@@ -16,3 +16,52 @@ btnTheme.addEventListener('click', () => {
   `;
   }
 });
+
+// API 
+const listCountries = document.getElementById('countries')
+
+document.addEventListener("DOMContentLoaded", e => {
+    fetchData()
+})
+
+const fetchData = async () => {
+    try {
+        const res = await fetch('https://restcountries.com/v3.1/all')
+        const data = await res.json()
+        console.log(data)
+        countries(data)
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const countries = data => {
+    let elements = ''
+    data.forEach(item => {
+        elements += `
+        <article class="card">
+            <img src="${item.flags.png}" alt="" class="img-fluid">
+            <div class="card-content">
+                <h3>${item.name.common}</h3>
+                <p>
+                    <b>Population: </b>
+                    ${item.population}
+                </p>
+                <p>
+                    <b>Capital: </b>
+                    ${item.capital}
+                </p>
+                <p>
+                    <b>Region: </b>
+                    ${item.region}
+                </p>
+                <p>
+                    <a href="country.html?name=${item.name.common}">More info</a>
+                </p>
+            </div>
+        </article>
+        `
+    });
+    listCountries.innerHTML = elements
+}
